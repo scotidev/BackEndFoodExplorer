745ba66bb660f2e73ddc9e7a9927049a1e8f4bb0
gabriel <gabrielscotio@gmail.com>
2024-06-05 09:51:38 -0300

    dependencies

--- src/server.js
+++ src/server.js
@@ -4,4 +4,9 @@ const app = express()
 
 const PORT = 3333
 
+// método get, acesse usando localhost:3333/message
+app.get("/message/:id", (request, response) => {
+    response.send('Hello')
+})
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
