2bf5e5bc1c6786b1aacd6b761be75f400176a233
gabriel <gabrielscotio@gmail.com>
2024-08-13 19:21:56 -0300

    apperror

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
 const express = require('express')
 
 const app = express()
@@ -7,6 +10,22 @@ app.use(express.json())
 // enable routes
 const routes = require("./routes")
 app.use(routes)
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 //porta do servidor
 const PORT = 3333
