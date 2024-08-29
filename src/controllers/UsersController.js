e1e8940d75974a98021142682df8a3e271197c4d
gabriel <gabrielscotio@gmail.com>
2024-08-29 12:32:59 -0300

    encrypting password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,58 @@
 const AppError = require("../utils/AppError")
+const { hash } = require("bcryptjs")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body
 
-        if(!name) {
-            throw new AppError("Nome nao existe")
+        const database = await sqliteConnection()
+
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(checkUserExists) {
+            throw new AppError("Este email já está sendo utilizado.")
         }
 
-        response.status(201).json({name, email, password})
+        const hashedPassword = await hash(password, 8)
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            [name, email, hashedPassword]
+        )
+
+        return response.status(201).json()
+    }
+
+    async update(request, response) {
+        const {name, email} = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado")
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este email já está em uso.")
+        }
+
+        user.name = name
+        user.email = email
+
+        await database.run(`
+            UPDATE users SET 
+            name = ?,
+            email = ?
+            WHERE id = ?`,
+            [user.name, user.email, id]
+        )
+
+        return response.json()
     }
 }
 
