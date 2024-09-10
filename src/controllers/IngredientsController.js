84f9203cee6c672b632113407655875f2463bab9
gabriel <gabrielscotio@gmail.com>
2024-09-10 19:39:48 -0300

    ingredients controller

---
+++ src/controllers/IngredientsController.js
@@ -0,0 +1,10 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+class IngredientsController {
+    async createIngredient(request, response) {
+        
+    }
+}
+
+module.exports = IngredientsController
