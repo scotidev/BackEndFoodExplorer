06e5ea691bd9d77b245763b0afde7ff5ea407dbc
gabrielscoti42 <gabrielscotio@gmail.com>
2025-08-09 16:39:07 -0300

    removed /

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://foodexplorer42.netlify.app/",
+      "https://foodexplorer42.netlify.app",
     ],
     credentials: true,
   })
